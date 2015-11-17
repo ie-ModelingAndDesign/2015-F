988c62806915174e0df282a54f79ac3792542619
kazuminn <warugaki_kazumi@docomo.ne.jp>
2015-11-17 18:43:27 +0900

    first commit

---
+++ 2015-FTests/_015_FTests.swift
@@ -0,0 +1,36 @@
+//
+//  _015_FTests.swift
+//  2015-FTests
+//
+//  Created by 木下　和巳 on 11/17/15.
+//  Copyright © 2015 ie-ModelingAndDesign. All rights reserved.
+//
+
+import XCTest
+@testable import _015_F
+
+class _015_FTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
