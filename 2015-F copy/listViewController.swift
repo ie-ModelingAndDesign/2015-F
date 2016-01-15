b8c19787252c1cb88e7058d1b31ea7043e81b949
kazuminn <e145702@ie.u-ryukyu.ac.jp>
2016-01-15 13:45:39 +0900

    add listViewController

---
+++ 2015-F copy/listViewController.swift
@@ -0,0 +1,27 @@
+//
+//  listViewController.swift
+//  2015-F
+//
+//  Created by 木下　和巳 on 1/15/16.
+//  Copyright © 2016 ie-ModelingAndDesign. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
